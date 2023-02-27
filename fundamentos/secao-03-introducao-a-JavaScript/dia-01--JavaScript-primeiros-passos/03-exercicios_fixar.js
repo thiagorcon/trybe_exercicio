5296badbc7ef2fa01c3a3d2321d57bd93bdf2a84
Thiago Rosa <thiagorcon@hotmail.com>
2023-02-27 16:12:37 -0300

    envio 03

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01--JavaScript-primeiros-passos/03-exercicios_fixar.js
@@ -0,0 +1,8 @@
+let nota = 55;
+
+if (nota >+ 80 && nota <= 100){
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+} else if (nota < 80 && nota >=60) { console.log("Parabéns, você faz parte do grupo de pessoas aprovadas");
+} else {
+  console.log("Infelizmente, você reprovou");
+}
