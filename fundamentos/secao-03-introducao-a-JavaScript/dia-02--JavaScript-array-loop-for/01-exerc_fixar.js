b2fb96ce0488656bac03cb8e84549a99f9e539c7
Thiago Rosa <thiagorcon@hotmail.com>
2023-02-28 11:23:40 -0300

    envio exercicio fixar 01

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02--JavaScript-array-loop-for/01-exerc_fixar.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let menuSevicos = menu.length
+let indexfPortifolio = menu.indexOf("Portfólio");
+letNovo = menu.push("tarefas");
+
+console.log(menuSevicos);
+console.log(indexfPortifolio);
+console.log(menu.length);
