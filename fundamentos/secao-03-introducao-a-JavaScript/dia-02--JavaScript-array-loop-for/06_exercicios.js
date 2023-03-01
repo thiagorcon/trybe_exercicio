df7b350581ec110689ed101f8911a8b4949bff27
Thiago Rosa <thiagorcon@hotmail.com>
2023-03-01 10:44:14 -0300

    envio lição 07

--- fundamentos/secao-03-introducao-a-JavaScript/dia-02--JavaScript-array-loop-for/06_exercicios.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02--JavaScript-array-loop-for/06_exercicios.js
@@ -3,12 +3,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
 
 
-for(let index = 0; index < numbers.length; index += 1){
-if(numbers[index] %2 !== 0)
-{
-resultado += 1;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    resultado += 1;
 
-} }if(resultado === 0) {
+  }
+} if (resultado === 0) {
   console.log("nenhum numero impar encontrado");
 } else {
   console.log(resultado);
